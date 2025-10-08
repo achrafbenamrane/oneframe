85385858f71130c0e42002df1dbd51c739d6bd37
achrafbenamrane <achrafbenamrane@proton.me>
2025-10-08 18:41:55 +0100

    add carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import OrderForm from "./compenents/Orderform"
+import ThreeDCarousel from "./compenents/ThreeDCarousel";
 
   
 export default function Home() {
@@ -9,12 +10,12 @@ export default function Home() {
 
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-6">
-       <div className=" p-4 rounded-xl">
-        <img src="/logo.png" alt="Logo" width={250} height={150} />
+       <div className=" pb-4 rounded-xl">
+        <img src="/logo.png" alt="Logo" width={180} height={105} />
       </div>
       <h1 className="flex justify-center items-center text-3xl text-gray-600  font-bold mb-2">MakeOrder </h1>
       <OrderForm/>
-      
+      <ThreeDCarousel/>
     </main>
   );
 }
